f75f6ab7cd9e9878becc93a91afc34cca60828bb
QIRIM-Young <16944106+QIRIM-Young@users.noreply.github.com>
2025-08-02 02:34:06 +0300

    +

--- dics/wordform_subdata.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b642b5107d9158e6d71d9aa036a485968aa608bc2bfed1e4ccffe794e9b11a6c
-size 15412962
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,19 +1,20 @@
 export const CONFIG = {
-  tries: 7, // This changes how many tries you get to finish the game
-  language: 'YourLanguageHere', // This changes the display name for your language
-  wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this game!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
-  //
-  // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
-  //
-  googleAnalytics: '', // You can use this if you use Google Analytics
-  shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
-  normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
-  startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
-  defaultLang: 'en', // the default interface language
-  availableLangs: ['en', 'es', 'sw', 'zh'], // the options available to the user for translation languages
-  escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
+  tries: 6, // Кількість спроб
+  language: 'Qırımtatarca', // Назва гри у шапці
+  wordLength: 5, // Довжина слова
+  author: "QIRI'M Young",
+  authorWebsite: 'https://ctcorpus.org',
+  wordListSource: 'Crimean Tatar National Corpus',
+  wordListSourceLink: 'https://ctcorpus.org',
+  
+  // Для аналітики Google Analytics. Поки залиште пустим.
+  googleAnalytics: '', 
+
+  // --- Розширені налаштування ---
+  shuffle: false, // Не перемішувати слова (щоб було "слово дня")
+  normalization: 'NFC', // Стандарт нормалізації Unicode
+  startDate: 'January 1, 2022 00:00:00',
+  defaultLang: 'crh', // Мова інтерфейсу за замовчуванням
+  availableLangs: ['crh', 'uk', 'tr', 'en'], // Мови, доступні для перемикання
+  escapeSpecialCharacters: true,
 }
